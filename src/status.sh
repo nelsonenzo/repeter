a82e4f92d439edeba279f20f8be529764ecadf7b
Nelson Hernandez <nelsonh@gmail.com>
2020-04-23 06:24:01 -0700

    fix flag

--- src/status.sh
+++ src/status.sh
@@ -1,7 +1,7 @@
 if [[ "$1" == 'status' ]]; then
   dPulumi=$REPETERDIR/src/pulumi
   pulumi --cwd=$dPulumi stack select $(CONFIG "pulumi_stack")
-  pulumi -cwd=$dPulumi stack
+  pulumi --cwd=$dPulumi stack
   cd $REPETERDIR/logs
   for logfile in $(ls $REPETERDIR/logs); do
     tail $REPETERDIR/logs/$logfile
